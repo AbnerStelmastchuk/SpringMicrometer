55e5802775d9ac7c8bed6db0058c5c5c85251b69
Abner Stelmastchuk Vieira <stelmastchukabner@gmail.com>
2022-12-02 12:09:10 -0300

    First commit

---
+++ src/test/java/com/example/SpringMicrometer/SpringMicrometerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringMicrometer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMicrometerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
